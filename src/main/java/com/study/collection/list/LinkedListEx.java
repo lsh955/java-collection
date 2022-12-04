514fe92f57da0da52653bf44eb703759db7e2671
이승환 <lsh01087674117@gmail.com>
2022-12-04 19:38:37 +0900

    class name 변경(Ex 는 example 를 줄임말)

--- src/main/java/com/study/collection/list/LinkedListEx.java
+++ src/main/java/com/study/collection/list/LinkedListEx.java
@@ -4,7 +4,7 @@ package com.study.collection.list;
  * @author 이승환
  * @since 2020-11-17
  */
-public class LinkedList {
+public class LinkedListEx {
 
     public static void main(String[] args) {
 
