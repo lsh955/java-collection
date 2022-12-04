dab83f0fef308e0a8e8739967f546adbc4cc170f
이승환 <lsh01087674117@gmail.com>
2022-12-04 10:43:09 +0900

    spring import 삭제

--- src/main/java/com/study/collection/CollectionApplication.java
+++ src/main/java/com/study/collection/CollectionApplication.java
@@ -1,8 +1,5 @@
 package com.study.collection;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 public class CollectionApplication {
 
     public static void main(String[] args) {
