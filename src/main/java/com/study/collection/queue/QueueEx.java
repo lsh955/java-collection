3caa03067cff4165a5c9bd8e06ca1cfa82d6db6c
이승환 <lsh01087674117@gmail.com>
2022-12-05 15:31:28 +0900

    class name명 변경

--- src/main/java/com/study/collection/queue/QueueEx.java
+++ src/main/java/com/study/collection/queue/QueueEx.java
@@ -4,10 +4,8 @@ package com.study.collection.queue;
  * @author 이승환
  * @since 2020-11-17
  */
-public class PriorityQueue {
-
+public class QueueEx {
     public static void main(String[] args) {
 
     }
-
 }
