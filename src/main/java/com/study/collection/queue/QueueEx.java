bd97047aef5233d76ba746ed4cdbba9ca7b1edbe
이승환 <lsh01087674117@gmail.com>
2022-12-05 16:15:45 +0900

    LinkedList 를 이용하여 큐 메모리 구조를 구현한 예제

--- src/main/java/com/study/collection/queue/QueueEx.java
+++ src/main/java/com/study/collection/queue/QueueEx.java
@@ -1,13 +1,32 @@
 package com.study.collection.queue;
 
+import java.util.LinkedList;
+
 /**
  * @author 이승환
  * @since 2020-11-17
  */
 public class QueueEx {
-
     public static void main(String[] args) {
+        // 큐 생성
+        LinkedList<String> queue = new LinkedList<String>();
 
+        // add() 메소드를 이용한 요소의 반환
+        queue.add("넷");
+        queue.add("둘");
+        queue.add("셋");
+        queue.add("하나");
+
+        // peek() 메소드를 이용한 제일먼저 저장된 요소의 반한
+        System.out.println(queue.peek());
+        System.out.println(queue);
+
+        // poll() 메소드를 이용한 제일먼저 저장된 요소의 반환 및 제거
+        System.out.println(queue.poll());
+        System.out.println(queue);
+
+        // remove() 메소드를 이용한 요소의 제거
+        queue.remove("하나");
+        System.out.println(queue);
     }
-
 }
