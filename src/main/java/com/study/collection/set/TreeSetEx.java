57acec0b54ac0e05ef01f5b676009b2db1042b0b
이승환 <lsh01087674117@gmail.com>
2022-12-05 19:02:08 +0900

    class rename

--- src/main/java/com/study/collection/set/TreeSetEx.java
+++ src/main/java/com/study/collection/set/TreeSetEx.java
@@ -4,10 +4,8 @@ package com.study.collection.set;
  * @author 이승환
  * @since 2020-11-17
  */
-public class TreeSet {
-
+public class TreeSetEx {
     public static void main(String[] args) {
 
     }
-
 }
